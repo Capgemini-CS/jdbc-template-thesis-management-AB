60b270cd624100f63a3e5c516d9d84f4459f522c
AndreeaaBadea <andreea.badea@capgemini.com>
2022-04-06 11:45:45 +0300

    Configure project

---
+++ src/main/java/com/jdbctemplatethesismanagement/JdbcTemplateThesisManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jdbctemplatethesismanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTemplateThesisManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcTemplateThesisManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdbctemplatethesismanagement/JdbcTemplateThesisManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbctemplatethesismanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTemplateThesisManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
